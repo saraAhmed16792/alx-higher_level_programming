a4fd06a5460596591dec4d4d60b6a2c64ca9b1cd
Sara <sara.a.imbaby@gmail.com>
2024-01-05 22:41:09 -0800

    0-print_list_integer.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return (None)
+    len = len(my_list)
+    if idx > len - 1:
+        return (None)
+    return(my_list[idx])
