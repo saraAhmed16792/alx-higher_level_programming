8956629a153d70c74b6d20b51f0c9be48cfaec71
Sara <sara.a.imbaby@gmail.com>
2024-02-13 14:20:41 +0000

    a

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a base model class."""
+class Base:
+    """Represent the base mod."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
