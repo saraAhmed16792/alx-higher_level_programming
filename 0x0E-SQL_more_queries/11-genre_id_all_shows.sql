d56feb244eac9549b6fc58b1660bd373609b6478
Sara <sara.a.imbaby@gmail.com>
2024-03-13 15:41:31 -0700

    11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
-- Lists all shows contained in the database hbtn_0d_tvshows.
+-- Lists all shows contained in the database hbtn_0d_tvshows.
 -- Displays NULL for shows without genres.
 -- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
 SELECT s.`title`, g.`genre_id`
